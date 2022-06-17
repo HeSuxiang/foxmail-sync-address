ecb30e0b909a1a0b34742322e62dfb05b1e21c38
hesuxiang <cuisanzhang@163.com>
2022-06-17 19:13:19 +0800

    v1.0 ok

--- foxmail-sync-address/Form1.Designer.cs
+++ foxmail-sync-address/Form1.Designer.cs
@@ -28,38 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(58, 215);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(239, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(413, 215);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -68,7 +39,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(482, 178);
+            this.textBox1.Size = new System.Drawing.Size(351, 115);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -76,15 +47,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 281);
+            this.ClientSize = new System.Drawing.Size(387, 146);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "中深foxmail同步地址簿工具 v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,9 +62,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- foxmail-sync-address/Form1.cs
+++ foxmail-sync-address/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using System.IO;
+
 //检测进程使用Process
 using System.Diagnostics;
 
@@ -13,12 +15,158 @@ namespace foxmail_sync_address
 {
     public partial class Form1 : Form
     {
+        //进程名
+        private string FoxmailProcessName = "Foxmail";
+        //文件名
+        private string FoxmailAppName = "Foxmail.exe";
+        
 
-        public string FoxmailProcessName = "Foxmail";
+        //服务器路径
+        string ServerPath = @"\\192.168.9.233\foxmail";
+
+        //本地foxmail文件夹路径
+        string FoxmailLocalPath = null;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //防止多开
+
+            //获取当前活动进程的模块名称
+            string moduleName = Process.GetCurrentProcess().MainModule.ModuleName;
+            //返回指定路径字符串的文件名
+            string processName = System.IO.Path.GetFileNameWithoutExtension(moduleName);
+            //根据文件名创建进程资源数组
+            Process[] processes = Process.GetProcessesByName(processName);
+            //如果该数组长度大于1，说明多次运行
+            if (processes.Length > 1)
+            {
+                //MessageBox.Show("本程序一次只能运行一个实例！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);//弹出提示信息
+                this.Close();//关闭当前窗体
+            }
+
+
+            //获取程序目录
+            //初始化foxmail本地路径
+            FoxmailLocalPath = System.Threading.Thread.GetDomain().BaseDirectory;
+            //开始同步
+            SyncFoxmailAddress();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SyncFoxmailAddress();
+        }
+
+
+        //同步地址薄文件夹
+        void SyncFoxmailAddress()
+        {
+            if (isFoxmailRuning())
+            {
+                ShowInfo("Foxmail运行中,本次不更新邮箱地址");
+                MessageBox.Show("Foxmail运行中,本次不更新邮箱地址");
+
+                //启动Foxmail
+                Process.Start(FoxmailAppName);
+                //退出程序
+                Application.Exit();
+            }
+
+
+            try
+            {
+                //SharedTool tool = new SharedTool("foxmail", "zs2022", "192.168.9.233");
+                //DebugSharedTool();
+                ShowInfo("开始更新邮箱地址");
+
+                SharedTool tool = new SharedTool("guest", "", "192.168.9.233");
+                CopyFolder(ServerPath, FoxmailLocalPath);
+
+                //更新后启动Foxmail
+                Process.Start(FoxmailAppName);
+            }
+            catch (Exception ex)
+            {
+                ShowInfo("连接到服务器时错误:" + ex.Message);
+            }
+
+            //退出程序
+            Application.Exit();
+        }
+
+        //调试输出
+        void DebugSharedTool()
+        {
+            var dicInfo = new DirectoryInfo(ServerPath);//选择的目录信息  
+
+            DirectoryInfo[] dic = dicInfo.GetDirectories("*.*", SearchOption.TopDirectoryOnly);
+            foreach (DirectoryInfo temp in dic)
+            {
+                Console.WriteLine(temp.FullName);
+            }
+
+            Console.WriteLine("---------------------------");
+            FileInfo[] textFiles = dicInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly);//获取所有目录包含子目录下的文件  
+            foreach (FileInfo temp in textFiles)
+            {
+                Console.WriteLine(temp.Name);
+            }
+
+        }
+
+
+        //https://www.cnblogs.com/fps2tao/p/14965561.html
+        //C#中复制文件夹及文件的两种方法
+        //方法一:
+        /// <summary>
+        /// 复制文件夹及文件
+        /// </summary>
+        /// <param name="sourceFolder">原文件路径</param>
+        /// <param name="destFolder">目标文件路径</param>
+        /// <returns></returns>
+        public int CopyFolder(string sourceFolder, string destFolder)
+        {
+            try
+            {
+                //如果目标路径不存在,则创建目标路径
+                if (!System.IO.Directory.Exists(destFolder))
+                {
+                    System.IO.Directory.CreateDirectory(destFolder);
+                }
+                //得到原文件根目录下的所有文件
+                string[] files = System.IO.Directory.GetFiles(sourceFolder);
+                foreach (string file in files)
+                {
+                    string name = System.IO.Path.GetFileName(file);
+                    string dest = System.IO.Path.Combine(destFolder, name);
+                    System.IO.File.Copy(file, dest, true);//复制文件, 允许覆盖同名的文件。
+                }
+                //得到原文件根目录下的所有文件夹
+                string[] folders = System.IO.Directory.GetDirectories(sourceFolder);
+                foreach (string folder in folders)
+                {
+                    string name = System.IO.Path.GetFileName(folder);
+                    string dest = System.IO.Path.Combine(destFolder, name);
+                    CopyFolder(folder, dest);//构建目标路径,递归复制文件
+                }
+                return 1;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                ShowInfo("复制文件时出错");
+                //抛出异常
+                throw (exception);
+
+            }
+
         }
 
 
@@ -62,7 +210,7 @@ namespace foxmail_sync_address
         {
             textBox1.SelectionStart = textBox1.Text.Length;
             textBox1.ScrollToCaret();
-      
+
         }
 
     }
